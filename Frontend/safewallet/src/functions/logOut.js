1b3a69a1215f8cd7ce1a7d73f99c0bb4f68c18bc
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2024-04-12 10:59:17 -0300

    (logOutFunction) fix it

--- Frontend/safewallet/src/functions/logOut.js
+++ Frontend/safewallet/src/functions/logOut.js
@@ -2,19 +2,18 @@ import axios from "axios";
 import { URL_APP, URL_LOGOUT } from "../URLS/URL";
 import decodedToken from "./decodedToken";
 
-export const logOut = () => {
+export const logOut = async () => {
   let data = JSON.parse(sessionStorage.getItem("user"));
   const token = sessionStorage.getItem("token");
-  let email = null
+  let email = null;
 
   // Si por algun motivo no podemos acceder al usuario que tiene la sesion iniciada, lo sacamos del token. Ya que los servicios son distintos y puede funcionar uno y otro no.
   if (!data) {
     data = decodedToken(token);
-    email = data.email
-  }
-    
     email = data.email;
+  }
 
+  email = data.email;
 
   const config = {
     headers: {
@@ -23,12 +22,12 @@ export const logOut = () => {
   };
 
   try {
-    axios.post(`${URL_LOGOUT}/${email}`, config).then((res) => {
-      if (res.status == 200) {
-        sessionStorage.clear();
-        location.replace("https://safewallet-teal.vercel.app/");
-      }
-    });
+    const response = await axios.post(`${URL_LOGOUT}/${email}`, config);
+    if (response.status === 200) {
+      sessionStorage.clear();
+      localStorage.clear();
+      location.replace(URL_APP);
+    }
   } catch (e) {
     console.log(e);
   }
