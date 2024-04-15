2bb0c1209da8067aef9c72eef5110b65d25eaa4a
Maximiliano Cattaneo Cvetic <101187172+MaxiCattaneoCvetic@users.noreply.github.com>
2024-04-15 15:51:05 -0300

    Update UseAuth.jsx

--- Frontend/safewallet/src/security/UseAuth.jsx
+++ Frontend/safewallet/src/security/UseAuth.jsx
@@ -26,7 +26,7 @@ const useAuth = () => {
       // Limpiar sessionStorage
       sessionStorage.clear();
       localStorage.clear();
-      location.replace("https://safewallet-teal.vercel.app");
+      location.replace("https://safewallet-sooty.vercel.app/");
       swal(
         "Sesión expirada",
         "Tu sesión ha expirado. Por favor, inicia sesión nuevamente.",
@@ -73,7 +73,7 @@ const useAuth = () => {
           "El servidor agotó el tiempo de espera, por favor contacta con un administrador. \n" +
             error,
           "error"
-        ).then(() => location.replace("https://safewallet-teal.vercel.app"));
+        ).then(() => location.replace("https://safewallet-sooty.vercel.app/"));
       });
   }, [navigate]);
 
