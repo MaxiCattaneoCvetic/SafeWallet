1e3c94c9200168e8af5073bae10739c123216d59
Maximiliano Cattaneo Cvetic <101187172+MaxiCattaneoCvetic@users.noreply.github.com>
2024-04-24 09:21:17 -0300

    Update SendMoney.jsx

--- SafeWallet/Frontend/safewallet/src/components/sendMoney/SendMoney.jsx
+++ SafeWallet/Frontend/safewallet/src/components/sendMoney/SendMoney.jsx
@@ -5,30 +5,40 @@ import swal from "sweetalert";
 import getUserFromData from "../../api/getUserFromData.js";
 // eslint-disable-next-line no-undef
 import formatNum from "format-num";
+import { SpinnerDotted } from "spinners-react";
 
 export default function SendMoney(props) {
   const [cbuTo, setCbu] = useState();
   const [cbuFrom, setCbuFrom] = useState(props.cbuFrom);
   const [alias, setAlias] = useState();
   const [amount, setAmount] = useState();
+  const [sending, setSending] = useState(false);
 
   async function swalFunction(response) {
     swal({
       title: `¿Enviar $${formatNum(amount)} a ${response.data}?`,
-      text: `Al aceptar enviará $${formatNum(amount)} a la cuenta de SafeWallet de  ${response.data}.`,
+      text: `Al aceptar enviará $${formatNum(
+        amount
+      )} a la cuenta de SafeWallet de  ${response.data}.`,
       icon: "warning",
       buttons: true,
       dangerMode: true,
     }).then(async (willDelete) => {
       if (willDelete) {
         let res = await sendMoneyApi(amount, cbuFrom, cbuTo);
-        if (res.status === 200) {
-          swal("Dinero enviado", {
-            icon: "success",
-          }).then(() => window.location.reload());
+        if (!res) {
+          setSending(true);
         } else {
-          swal("No se envio el dinero \n Error: ", res.response.data);
-          return;
+          if (res.status === 200) {
+            setSending(false);
+            swal("Dinero enviado", {
+              icon: "success",
+            }).then(() => window.location.reload());
+          } else {
+            setSending(false);
+            swal("No se envio el dinero \n Error: ", res.response.data);
+            return;
+          }
         }
       } else {
         swal("Se canceló la operación, tu dinero esta a salvo", {
@@ -56,36 +66,51 @@ export default function SendMoney(props) {
 
   return (
     <>
-      <div className={style.formContainer}>
-        <img src="/sendMoney.png" alt="sendMoneyIcon" />
-        <form action="" className={style.formInput}>
-          <label className={style.labelForm} htmlFor="">
-            ¿A quién le vas a transferir?
-          </label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setCbu(e.target.value);
-            }}
-            placeholder="CBU, CVU o Alias"
-            required
-          />
-          <label className={style.labelForm} htmlFor="">
-            ¿Cuanto vas a transferir?
-          </label>
-          <input
-            type="number"
-            className="sinflechas"
-            onChange={(e) => {
-              setAmount(e.target.value);
-            }}
-            placeholder="Ingrese el monto $"
-            required
-          />
-          <button className="primarybtn" type="submit" onClick={handleSubmit}>
-            Enviar dinero
-          </button>
-        </form>
+      <div>
+        {sending ? (
+          <div>
+            <SpinnerDotted />
+            <p>Enviando dinero...</p>
+          </div>
+        ) : (
+          <>
+            <div className={style.formContainer}>
+              <img src="/sendMoney.png" alt="sendMoneyIcon" />
+              <form action="" className={style.formInput}>
+                <label className={style.labelForm} htmlFor="">
+                  ¿A quién le vas a transferir?
+                </label>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setCbu(e.target.value);
+                  }}
+                  placeholder="CBU, CVU o Alias"
+                  required
+                />
+                <label className={style.labelForm} htmlFor="">
+                  ¿Cuanto vas a transferir?
+                </label>
+                <input
+                  type="number"
+                  className="sinflechas"
+                  onChange={(e) => {
+                    setAmount(e.target.value);
+                  }}
+                  placeholder="Ingrese el monto $"
+                  required
+                />
+                <button
+                  className="primarybtn"
+                  type="submit"
+                  onClick={handleSubmit}
+                >
+                  Enviar dinero
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
